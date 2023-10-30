5b7e1e46a8a96c61f4d7c6f1c82689be063af87c
Diogo Pereira <diogopereira.dev@gmail.com>
2023-10-30 00:25:45 -0300

    feat: Getting Started section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,7 @@ import Header from '~/app/home/Header.vue';
 import FeaturesCards from '~/app/home/FeaturesCards.vue';
 import MainFeatures from '~/app/home/MainFeatures.vue';
 import Customizations from '~/app/home/Customizations.vue';
+import GettingStarted from '~/app/home/GettingStarted.vue';
 </script>
 
 <template>
@@ -14,5 +15,6 @@ import Customizations from '~/app/home/Customizations.vue';
     <FeaturesCards />
     <MainFeatures />
     <Customizations />
+    <GettingStarted />
   </div>
 </template>
